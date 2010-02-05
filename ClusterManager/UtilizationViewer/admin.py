34517c6e1afe5311d3cc10a13a13564bd16e36d7
Prashanth Mohan <prashmohan@gmail.com>
2010-02-05 00:43:07 -0800

    Removing admin file for Utilization Viewer

--- ClusterManager/UtilizationViewer/admin.py
+++
@@ -1,7 +0,0 @@
-from ClusterManager.UtilizationViewer.models import User, Node, ACme, ACmeBinding
-from django.contrib import admin
-
-admin.site.register(User)
-admin.site.register(Node)
-admin.site.register(ACme)
-admin.site.register(ACmeBinding)
